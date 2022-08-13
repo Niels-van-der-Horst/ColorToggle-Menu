769d8c334fc157085cf31c58cd7ccb9a83f42de6
Niels van der Horst <101106603+Niels-van-der-Horst@users.noreply.github.com>
2022-08-13 23:36:00 +0200

    Add files via upload
    initial commit

---
+++ JStoggleColor 2/js/script.js
@@ -0,0 +1,82 @@
+// Keyboard color event
+
+document.addEventListener("keyup", function(event) {
+ 
+    let key = event.key;
+   
+    switch (key) {
+        case '1':
+            colorBG('g');
+            break;
+        case '2':
+            colorBG('r');
+            break;
+        case '3':
+            colorBG('o');
+            break;
+        case '4':
+            colorBG('p');
+            break;
+        case '5':
+            colorBG('gr');
+            break;
+        default:
+            console.log("No keypress yet!");    
+
+    }
+
+});
+
+
+// Display Menu onClick
+
+
+function showMenu () {
+    var x = document.getElementById("menu-list");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+
+    } else {
+        x.style.display = "block";
+    }
+}
+function colorBG(a) {
+    let secColor = document.querySelector("body");
+    
+            if (a=="g")
+            {
+                secColor.style.backgroundColor = "grey";
+                closeMenu();
+            }
+
+            if (a=="r")
+            {
+                secColor.style.backgroundColor = "red";
+                closeMenu();
+            }
+            if (a=="o")
+            {
+                secColor.style.backgroundColor = "orange";
+                closeMenu();
+            }
+            if (a=="p")
+            {
+                secColor.style.backgroundColor = "purple";
+                closeMenu();
+            }
+            if (a=="gr")
+            {
+                secColor.style.backgroundColor = "green";
+                closeMenu();
+            }
+            
+
+// Automatic close Menu    
+
+}
+function closeMenu() {
+  const menuContainer =  document.querySelector('#menu-list');
+  menuContainer.style.display = "none";
+
+}
+
